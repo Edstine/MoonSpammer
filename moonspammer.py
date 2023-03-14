b6d0afaa07344883a4c299dd422a4edac822ac5d
Edstine <93057367+Edstine@users.noreply.github.com>
2023-03-14 21:14:44 +0000

    Update moonspammer.py

--- moonspammer.py
+++ moonspammer.py
@@ -22,40 +22,44 @@ print(Colorate.Vertical(Colors.purple_to_blue, """
        ░  ░      ░  ░ ▒ ▒░   ░ ▒ ▒░ ░ ░░   ░ ▒░   ░ ░▒  ░ ░░▒ ░       ▒   ▒▒ ░░  ░      ░░  ░      ░ ░ ░  ░  ░▒ ░ ▒░
        ░      ░   ░ ░ ░ ▒  ░ ░ ░ ▒     ░   ░ ░    ░  ░  ░  ░░         ░   ▒   ░      ░   ░      ░      ░     ░░   ░ 
               ░       ░ ░      ░ ░           ░          ░                 ░  ░       ░          ░      ░  ░   ░     
-                                          Discord : dsc.gg/team-camex""", 1))
+                                          By Edstine#6666""", 1))
 
 
-proxy = ""
-with open("proxies.txt",'r') as f:
-    proxy = "http://" + choice(f.readlines()).strip()
 
 print()
-webhook = input(Colorate.Horizontal(Colors.purple_to_blue, "     [?] Enter the Webhook Url: ", 1))
-msg = input(Colorate.Horizontal(Colors.purple_to_blue, "     [?] Enter the message: ", 1))
+os.system("title Moon Spammer V1.1 ^| Github.com/Edstine ")
+webhook = input(Fore.WHITE + "     [" + Fore.MAGENTA + "?"  + Fore.WHITE + "]" + " Enter the Webhook Url: " + Fore.MAGENTA)
+msg = input(Fore.WHITE + "     [" + Fore.MAGENTA + "?"  + Fore.WHITE + "]" + " Enter the message: " + Fore.MAGENTA)
 
 
-def proxy_spam():
-        req = httpx.post(webhook, json={'content': msg},proxies=proxy)
+def spam():
+        req = httpx.post(webhook, json={'content': msg})
         if req.status_code == 204:
             print(Fore.GREEN+"     [+] Message Sent to the webhook Successfully")
         else:
             print(Fore.RED+"     [-] Bad proxies or webhook or ratelimited")
             
-def proxyless_spam():
-        req = httpx.post(webhook, json={'content': msg})
-        if req.status_code == 204:
-            print(Fore.GREEN+"     [+] Message Sent to the webhook Successfully")
-        else:
-            print(Fore.RED+"     [-] Bad webhook or ratelimited")
-            
 
-support = input(Fore.LIGHTYELLOW_EX+"     [?] Do you want to enable proxy mode?(y/n): ")
+support = input(Fore.LIGHTYELLOW_EX+"     [?] Do you want to delete the webhook after?(y/n): ")
 
-if support == "y":
-    for i in range(50):
-        proxy_spam()
-elif support == "n":
-    for i in range(50):
-        proxyless_spam()
+if support == "n":
+    for i in range(25):
+        spam()
+    cls()
+    print()
+    print(Fore.WHITE + "    [" + Fore.MAGENTA + "+"  + Fore.WHITE + "]" + " WEBHOOK IS SPAMMED, PRESS ENTER TO JOIN DISCORD !" + Fore.MAGENTA)
+    input()
+    os.system("start https://teamcamex.fr/discord")
+    input()
+elif support == "y":
+    for i in range(25):
+        spam()
+    os.system("CURL -X \"DELETE\" " + webhook)
+    cls()
+    print()
+    print(Fore.WHITE + "    [" + Fore.MAGENTA + "+"  + Fore.WHITE + "]" + " WEBHOOK IS SPAMMED / DELETED, PRESS ENTER TO JOIN DISCORD !" + Fore.MAGENTA)
+    input()
+    os.system("start https://teamcamex.fr/discord")
+    input()
 else:
     sys.exit()
